e6e0b459eccbe1761fc622b5b647d1f5407de99d
Dulinag <83606238+Dulinag@users.noreply.github.com>
2023-02-10 18:42:17 -0500

    Add files via upload

--- activity4.cs
+++ activity4.cs
@@ -1,1 +1,46 @@
+using System;
+namespace MyNamespace {
 
+
+    public class Number {
+        public int value;
+    
+    };
+
+    public class Counter
+    {
+        public event NumberValidator Assigned;
+
+        protected virtual void OnAssigned()
+        {
+            if(NumberValidator !=null) {
+                NumberValidator(this,null);
+            }
+        }
+
+    }
+
+    public class AppService
+    {
+        public void CorrecctAssign(int value)
+        {
+            console.WriteLine("App Service: Dulina Your number is valid")
+        }
+    }
+
+    class program
+    {
+
+        static  Main(string[] args)
+        {
+
+
+            var number = new Number { value= 1 };   
+            var CorrectCounter = new Counter();
+            var apps = new AppService();
+            CorrectCounter.Assigned += apps.CorrecctAssign;
+            Console.ReadKey();
+
+        }
+    }
+}
